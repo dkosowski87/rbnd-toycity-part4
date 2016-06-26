ccbd77e4a3dad0193bcc47ff1ef2924135314663
Damian Kosowski <kosowski.d@gmail.com>
2016-06-26 23:38:05 +0200

    Add methods for creating and saving udacidata objects

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,5 +3,28 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
-  # Your code goes here!
+
+	DATA_FILE = File.dirname(__FILE__) + "/../data/data.csv"
+
+	def self.create(options={})
+		ud_object = new(options)
+		ud_object.send(:set_attributes, options)
+		ud_object.send(:save)
+	end
+
+	private
+	def set_attributes(attributes)
+		if attributes[:name]
+			attributes[:"#{self.class.to_s.downcase}"] = attributes.delete(:name)
+		end
+		@attributes = attributes.merge(id: id)
+	end
+
+	def save
+		CSV.open(DATA_FILE, 'a+b') do |csv|
+			csv << csv.first.map { |field| @attributes[field.to_sym] }
+		end
+		return self
+	end
+
 end
