bd58b06a6f31a3446fe2abb834a9ea531b014f85
Damian Kosowski <kosowski.d@gmail.com>
2016-07-01 11:49:46 +0200

    Add udacidata all method

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -7,24 +7,30 @@ class Udacidata
 	DATA_FILE = File.dirname(__FILE__) + "/../data/data.csv"
 
 	def self.create(options={})
-		ud_object = new(options)
-		ud_object.send(:set_attributes, options)
-		ud_object.send(:save)
+		save new(options)
 	end
 
-	private
-	def set_attributes(attributes)
-		if attributes[:name]
-			attributes[:"#{self.class.to_s.downcase}"] = attributes.delete(:name)
-		end
-		@attributes = attributes.merge(id: id)
+	def self.all
+		data = CSV.read(DATA_FILE, {converters: :float})
+		attributes = convert_attributes(data.delete_at(0))
+		data.map! { |values| new(attributes.zip(values).to_h) }
 	end
 
-	def save
+	#Internal:
+
+	def self.save(udacidata_object)
 		CSV.open(DATA_FILE, 'a+b') do |csv|
-			csv << csv.first.map { |field| @attributes[field.to_sym] }
-		end
-		return self
+			attributes = convert_attributes(csv.first)
+			csv << attributes.map { |field| udacidata_object.send(field) }
+		end and return udacidata_object
 	end
+
+	def self.convert_attributes(attributes)
+		object_name_index = attributes.index(to_s.downcase)
+		attributes[object_name_index] = 'name' if object_name_index
+		attributes.map(&:to_sym)
+	end
+
+	private_class_method :save, :convert_attributes
 
 end
