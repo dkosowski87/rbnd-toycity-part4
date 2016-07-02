15934a77d4ecd929572dc2bf9be5ef44f79f2468
Damian Kosowski <kosowski.d@gmail.com>
2016-07-02 16:06:19 +0200

    Refactor code

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -7,52 +7,57 @@ class Udacidata
 	DATA_FILE = File.dirname(__FILE__) + "/../data/data.csv"
 
 	def self.create(options={})
-		save new(options)
+		new_object = new(options)
+		data = read_from_database
+		data << attributes(data.first).map { |field| new_object.send(field) }
+		write_to_database(data)
+		return new_object
 	end
 
 	def self.all
-		data, attributes = upload_data
-		data.map { |values| new(attributes.zip(values).to_h) }
+		data = read_from_database
+		data.drop(1).map { |values| create_object(data.first, values) }
 	end	
 
 	def self.first(n=1)
-		data, attributes = upload_data
-		ud_data = data.first(n).map { |values| new(attributes.zip(values).to_h) }
+		data = read_from_database
+		ud_data = data.drop(1).first(n).map { |values| create_object(data.first, values) }
 		n == 1 ? ud_data.first : ud_data
 	end
 
 	def self.last(n=1)
-		data, attributes = upload_data
-		ud_data = data.last(n).map { |values| new(attributes.zip(values).to_h) }
+		data = read_from_database
+		ud_data = data.drop(1).last(n).map { |values| create_object(data.first, values) }
 		n == 1 ? ud_data.first : ud_data		
 	end
 
 	def self.find(id)
-		data, attributes = upload_data
-		new attributes.zip(data.find { |row| row[0] == id }).to_h
+		data = read_from_database
+		new attributes(data.first).zip(data.find { |row| row[0] == id }).to_h
 	end
 
 	#Internal:
 
-	def self.save(udacidata_object)
-		CSV.open(DATA_FILE, 'a+b') do |csv|
-			attributes = convert_attributes(csv.first)
-			csv << attributes.map { |field| udacidata_object.send(field) }
-		end and return udacidata_object
+	def self.read_from_database
+		CSV.read(DATA_FILE, {converters: :float})
 	end
 
-	def self.upload_data
-		data = CSV.read(DATA_FILE, {converters: :float})
-		attributes = convert_attributes(data.delete_at(0))
-		return [data, attributes]
+	def self.create_object(headers, values)
+		new attributes(headers).zip(values).to_h
 	end
 
-	def self.convert_attributes(attributes)
-		object_name_index = attributes.index(to_s.downcase)
-		attributes[object_name_index] = 'name' if object_name_index
-		attributes.map(&:to_sym)
+	def self.write_to_database(data)
+		CSV.open(DATA_FILE, 'wb') do |csv|
+			data.each { |row| csv << row }
+		end
 	end
 
-	private_class_method :save, :upload_data, :convert_attributes
+	def self.attributes(headers)
+		object_name_index = headers.index(to_s.downcase)
+		headers[object_name_index] = 'name' if object_name_index
+		headers.map(&:to_sym)
+	end
+
+	private_class_method :read_from_database, :write_to_database, :attributes
 
 end
