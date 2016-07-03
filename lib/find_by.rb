e81c27e15b1333bf419639de30243544445aded2
Damian Kosowski <kosowski.d@gmail.com>
2016-07-03 19:59:55 +0200

    Create find_by methods

--- lib/find_by.rb
+++ lib/find_by.rb
@@ -1,6 +1,21 @@
 class Module
   def create_finder_methods(*attributes)
-    # Your code goes here!
-    # Hint: Remember attr_reader and class_eval
+  	attributes.each do |attr_name|
+  		instance_eval(<<-DEFINING_METHODS, __FILE__, __LINE__ + 1)
+  			def find_by_#{attr_name}(attr_value)
+  				data = read_from_database
+  				value_index = attributes(data.first).index(:#{attr_name})
+  				values = data.drop(1).find { |row| row[value_index] == attr_value }	
+					create_object(data.first, values)			
+  			end
+  		DEFINING_METHODS
+  	end
+  end
+
+  def attr_reader(*attributes)
+    create_finder_methods *attributes
+    attributes.each do |attribute|
+      class_eval "def #{attribute}; @#{attribute}; end"
+    end
   end
 end
